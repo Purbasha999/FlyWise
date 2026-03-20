d1cc82ef0d94e15b0c5f3e9090b6950c40486be5
Kanka Oza <kankaoza1609@gmail.com>
2026-03-20 12:24:46 +0530

    addons section complete

--- frontend/src/pages/AddOnsSection.js
+++ frontend/src/pages/AddOnsSection.js
@@ -19,17 +19,20 @@ export default function AddOnsSection({selected,setSelected}) {
   }, [tab, veg]);
 
   const fetchAddons = async () => {
-    try {
-      const params = { type: tab };
-      if (veg !== null) params.veg = veg;
+  try {
+    const params = { type: tab };
 
-      const res = await getAddOns(params);
-      setAddons(res.data);
-      console.log(res.data)
-    } catch (err) {
-      console.error(err);
+    if (tab === "meal" && veg !== null) {
+      params.veg = veg;
     }
-  };
+
+    const res = await getAddOns(params);
+    setAddons(res.data);
+    console.log("DATA:", res.data);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
   const toggleAdd = (item) => {
     setSelected((prev) =>
@@ -58,37 +61,71 @@ export default function AddOnsSection({selected,setSelected}) {
 
       {/* Veg filter */}
       {tab === "meal" && (
-        <div className="addons-filter">
-          <button onClick={() => setVeg(true)}>Veg</button>
-          <button onClick={() => setVeg(false)}>Non-Veg</button>
-          <button onClick={() => setVeg(null)}>All</button>
-        </div>
-      )}
+  <div className="addons-filter">
+    <button
+      className={`filter-btn ${veg === true ? "active veg" : ""}`}
+      onClick={() => setVeg(true)}
+    >
+      <span className="dot veg"></span>
+      Veg
+    </button>
+
+    <button
+      className={`filter-btn ${veg === false ? "active nonveg" : ""}`}
+      onClick={() => setVeg(false)}
+    >
+      <span className="dot nonveg"></span>
+      Non-Veg
+    </button>
+
+    <button
+      className={`filter-btn ${veg === null ? "active" : ""}`}
+      onClick={() => setVeg(null)}
+    >
+      All
+    </button>
+  </div>
+)}
 
       {/* Cards */}
-      <div className="addons-grid">
-        {addons.map((item) => (
-          <div key={item._id} className="addon-card">
-            
-            {item.type === "baggage" ? (
-              <h3>{item.baggageWeight} kg Extra</h3>
-            ) : (
+      <div className="addons-list">
+  {addons.map((item) => {
+    const isSelected = selected.find(i => i._id === item._id);
+
+    return (
+      <div key={item._id} className={`addon-row ${isSelected ? "selected" : ""}`}>
+        
+        {/* LEFT */}
+        <div className="addon-img-wrap">
+          {item.type === "meal" ? (
+            <>
               <img src={item.image} alt="" />
-            )}
+              <span className={`veg-dot ${item.veg ? "veg" : "nonveg"}`} />
+            </>
+          ) : (
+            <div className="baggage-box">{item.baggageWeight}kg</div>
+          )}
+        </div>
 
-            <div className="addon-info">
-              <h4>{item.name}</h4>
-              <p>₹{item.price}</p>
-            </div>
+        {/* CENTER */}
+        <div className="addon-info">
+          <div className="addon-name">{item.name}</div>
+          <div className="addon-price">₹{item.price}</div>
+          <div className="addon-more">More info</div>
+        </div>
 
-            <button onClick={() => toggleAdd(item)}>
-              {selected.find((i) => i._id === item._id)
-                ? "Remove"
-                : "Add"}
-            </button>
-          </div>
-        ))}
+        {/* RIGHT */}
+        <button
+          className={`addon-btn ${isSelected ? "added" : ""}`}
+          onClick={() => toggleAdd(item)}
+        >
+          {isSelected ? "Added" : "Add"}
+        </button>
+
       </div>
+    );
+  })}
+</div>
     </div>
   );
 }
